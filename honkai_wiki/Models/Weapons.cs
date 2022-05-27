c83894aea51c41db14c498b4b9b04dc195c9e406
tehtko <moderndsnsam@gmail.com>
2022-05-26 21:02:49 -0400

    Create Weapon Model

---
+++ honkai_wiki/Models/Weapons.cs
@@ -0,0 +1,14 @@
+﻿namespace honkai_wiki.Models
+{
+    public class Weapons
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int BaseAtk { get; set; }
+        public int MaxAtk { get; set; }
+        public int BaseCrt { get; set; }
+        public int MaxCrt { get; set; }
+        public string Description { get; set; }
+        public string Skills { get; set; }
+    }
+}
