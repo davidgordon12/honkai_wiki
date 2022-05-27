060d9a2fb64d749bec551c41943e4d627a56bcb5
tehtko <moderndsnsam@gmail.com>
2022-05-26 21:16:49 -0400

    Create Valkyrie model

---
+++ honkai_wiki/Models/Valkyrie.cs
@@ -0,0 +1,10 @@
+﻿namespace honkai_wiki.Models
+{
+    public class Valkyrie
+    {
+        public string Name { get; set; }
+        public Weapons Weapon { get; set; }
+        public string Description { get; set; }
+        public List<Battlesuit> Battlesuits { get; set; }
+    }
+}
