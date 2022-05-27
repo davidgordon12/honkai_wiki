2d2fbb9bbfd1c71ba82b1596d5b9cf47f64e498b
tehtko <moderndsnsam@gmail.com>
2022-05-26 21:16:39 -0400

    Create Battlesuit model

---
+++ honkai_wiki/Models/Battlesuit.cs
@@ -0,0 +1,10 @@
+﻿namespace honkai_wiki.Models
+{
+    public class Battlesuit
+    {
+        public string Name { get; set; }
+        public string Rank { get; set; }
+        public string Features { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
