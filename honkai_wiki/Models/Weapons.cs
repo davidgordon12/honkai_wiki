552e3daf99abe2f76927e25e756f39c7e1c5558b
tehtko <moderndsnsam@gmail.com>
2022-05-26 21:17:08 -0400

    Update Weapon model

--- honkai_wiki/Models/Weapons.cs
+++ honkai_wiki/Models/Weapons.cs
@@ -4,11 +4,12 @@
     {
         public string Name { get; set; }
         public string Type { get; set; }
+        public int Stars { get; set; }
         public int BaseAtk { get; set; }
         public int MaxAtk { get; set; }
         public int BaseCrt { get; set; }
         public int MaxCrt { get; set; }
         public string Description { get; set; }
-        public string Skills { get; set; }
+        public List<Skill> Skills { get; set; }
     }
 }
