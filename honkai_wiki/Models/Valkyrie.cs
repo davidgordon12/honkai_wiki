54ce22953c742332c595b1b371ad0e2a0ecce815
tehtko <moderndsnsam@gmail.com>
2022-06-05 19:47:37 -0400

    Re-create models

---
+++ honkai_wiki/Models/Valkyrie.cs
@@ -0,0 +1,11 @@
+﻿namespace honkai_wiki_api.Models
+{
+    public class Valkyrie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int Weapon { get; set; }
+        public string Description { get; set; }
+    }
+}
