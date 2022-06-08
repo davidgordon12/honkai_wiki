9fa3ba557690b3926ecad4b3e28991dbb738a7cf
tehtko <moderndsnsam@gmail.com>
2022-06-07 23:27:38 -0400

    Create services

---
+++ honkai_wiki/Services/IValkyrieService.cs
@@ -0,0 +1,9 @@
+﻿using honkai_wiki_api.Models;
+
+namespace honkai_wiki.Services
+{
+    public interface IValkyrieService
+    {
+        Task<List<Valkyrie>> GetValkyries();
+    }
+}
---
+++ honkai_wiki/Services/ValkyrieService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+using honkai_wiki_api.Models;
+
+namespace honkai_wiki.Services
+{
+    public class ValkyrieService : IValkyrieService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ValkyrieService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Valkyrie>> GetValkyries()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Valkyrie>>("https://localhost:7061/valkyries");
+        }
+    }
+}
