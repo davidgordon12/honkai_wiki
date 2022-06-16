79d3287d7c552a919adbfdd1f67e93bcd807edcd
tehtko <moderndsnsam@gmail.com>
2022-06-15 21:45:43 -0400

    Create IHonkaiService

---
+++ honkai_wiki/Services/IHonkaiService.cs
@@ -0,0 +1,11 @@
+﻿using honkai_wiki_api.Models;
+using Newtonsoft.Json.Linq;
+
+namespace honkai_wiki.Services
+{
+    public interface IHonkaiService
+    {
+        Task<string> GetValkyriesAsync();
+        Task<string> GetValkyrieAsync(int id);
+    }
+}
