327f9ffe6e16e6705beb018a62016ce1317cf65e
tehtko <moderndsnsam@gmail.com>
2022-06-19 20:34:39 -0400

    Create IWeaponService Interface

---
+++ honkai_wiki/Services/IWeaponService.cs
@@ -0,0 +1,11 @@
+﻿using honkai_wiki_api.Models;
+using Newtonsoft.Json.Linq;
+
+namespace honkai_wiki.Services
+{
+    public interface IWeaponService
+    {
+        Task<string> GetAsync();
+        Task<string> GetAsync(int id);
+    }
+}
