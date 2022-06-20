f72928ecfdcdfb3b8f67ea83d59d345cc726c5ff
tehtko <moderndsnsam@gmail.com>
2022-06-19 20:34:33 -0400

    Create IStigmataService Interface

---
+++ honkai_wiki/Services/IStigmataService.cs
@@ -0,0 +1,11 @@
+﻿using honkai_wiki_api.Models;
+using Newtonsoft.Json.Linq;
+
+namespace honkai_wiki.Services
+{
+    public interface IStigmataService
+    {
+        Task<string> GetAsync();
+        Task<string> GetAsync(int id);
+    }
+}
